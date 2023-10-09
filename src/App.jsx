3bfef3b9d61d2b2ed9fb6f64c9b770920039251b
selvaweb95 <selvaweb95@gmail.com>
2023-10-09 16:16:44 +0530

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signout from './pages/Signout';
+import About from './pages/About';
 export default function App() {
   return (
-    <div>
-      <h1 className="text-gray-500">App</h1>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/Profile" element={<Profile/>}></Route>
+          <Route path="/About" element={<About/>}></Route>
+          <Route path="/Signin" element={<Signin/>}></Route>
+          <Route path="/Signout" element={<Signout/>}></Route>
+
+        </Routes>
+    </BrowserRouter>
+    
   );
 }
